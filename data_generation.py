9fa51bd2dec8e17c6a71a2567008621908295648
MiaoDragon <innocentmyl@gmail.com>
2019-12-09 14:59:21 -0800

    edited data generation for home

--- data_generation.py
+++ data_generation.py
@@ -30,6 +30,7 @@ def randQ(N):
     #Generates a uniform random quaternion
     #James J. Kuffner 2004
     #A random array 3xN
+    """
     s = np.random.rand(3,N)
     sigma1 = np.sqrt(1.0 - s[0])
     sigma2 = np.sqrt(s[0])
@@ -40,6 +41,14 @@ def randQ(N):
     y = np.cos(theta1)*sigma1
     z = np.sin(theta2)*sigma2
     return np.array([w, x, y, z])
+
+    """
+    # generate random quarternion that is rotation around (0,0,1)
+    s = np.random.rand(N)
+    theta = 2*np.pi*s
+    C = np.cos(theta/2)
+    S = np.sin(theta/2)
+    return np.array([C,0*S,0*S,S])
 
 def QtoAxisAngle(Q):
     # angle = 2 * acos(qw)
@@ -55,12 +64,24 @@ def QtoAxisAngle(Q):
 
 ###############################################################################
 
-
+starts = np.loadtxt('start.txt')
+starts = starts[:75]
+#np.savetxt('start_75.txt', starts, fmt='%f')
+#np.savetxt('goal.txt', goals, fmt='%f')
 
 # define start state boundary
-start_bound = np.array([[184.95, -70, 49], [295, -330, 120]])
 
-for i in range(5000):
+start_goal = []
+for i in range(len(starts)):
+    for j in range(i+1, len(starts)):
+        start_goal.append( (i,j) )
+        # backward plan is the same as forward plan, so we don't need to plan twice
+print('number of path queries: ')
+print(len(start_goal))
+#for i in range(len(start_goal)):
+for i in range(len(start_goal)):
+    print(start_goal[i])
+    # try to plan EXACT_SOLUTION
     # generate 5000 paths in total
     setup = oa.SE3RigidBodyPlanning()
 
@@ -74,50 +95,65 @@ for i in range(5000):
     # setting collision checking resolution to 1% of the space extent
     setup.getSpaceInformation().setStateValidityCheckingResolution(0.01)
 
-    setup.setPlanner(allocatePlanner(setup.getSpaceInformation(), "rrtstar"))
+    setup.setPlanner(allocatePlanner(setup.getSpaceInformation(), "informedrrtstar"))
     # we call setup just so print() can show more information
 
-    goal = ob.State(setup.getSpaceInformation())
-    goal().setX(262.95)
-    goal().setY(75.05)
-    goal().setZ(46.19)
-    goal().rotation().setIdentity()
-    setup.setGoalState(goal)
-    setup.setup()
-    print(setup)
-
-
-
-    # setup random start and goal, and check if the start and goal are in collision
-    val = setup.getStateValidityChecker()
 
     print('generating path %d...' % (i))
 
-    while True:
-        # define start state
-        start_vec = np.random.uniform(low=start_bound[0], high=start_bound[1])
-        start_ori = randQ(1)[:,0]
-        # convert from quarternion to Axis Angle Representation
-        start_ori = QtoAxisAngle(start_ori)
+    # define start state
+    # load start and goal from file
+    start_i = start_goal[i][0]
+    goal_i = start_goal[i][1]
 
-        start = ob.State(setup.getSpaceInformation())
-        start().setX(start_vec[0])
-        start().setY(start_vec[1])
-        start().setZ(start_vec[2])
-        start().rotation().setAxisAngle(start_ori[0], start_ori[1], start_ori[2], start_ori[3])
+    start_vec = starts[start_i][:3]
+    goal_vec = starts[goal_i][:3]
+    start_ori = starts[start_i][3:]
+    goal_ori = starts[goal_i][3:]
+    # from OMPL Quarternion to AxisAngle, then to OMPL Quarternion
+    start_ori = [start_ori[3], start_ori[0], start_ori[1], start_ori[2]]
+    goal_ori = [goal_ori[3], goal_ori[0], goal_ori[1], goal_ori[2]]
+    start_ori = np.array(start_ori)
+    goal_ori = np.array(goal_ori)
+    # convert from quarternion to Axis Angle Representation
+    start_ori = QtoAxisAngle(start_ori)
+    goal_ori = QtoAxisAngle(goal_ori)
+    start = ob.State(setup.getSpaceInformation())
+    start().setX(start_vec[0])
+    start().setY(start_vec[1])
+    start().setZ(start_vec[2])
+    start().rotation().setAxisAngle(start_ori[0], start_ori[1], start_ori[2], start_ori[3])
 
-        if val.isValid(start()):
-            break
     # set the start states
     setup.setStartState(start)
 
+    goal = ob.State(setup.getSpaceInformation())
+    goal().setX(goal_vec[0])
+    goal().setY(goal_vec[1])
+    goal().setZ(goal_vec[2])
+    goal().rotation().setAxisAngle(goal_ori[0], goal_ori[1], goal_ori[2], goal_ori[3])
+
+    # set the goal states
+    setup.setGoalState(goal)
+
+    setup.setup()
+
+    print("start:")
+    print(start)
+    print("goal:")
+    print(goal)
+
     # try to solve the problem
-    solve = setup.solve(60)
-    if solve:
+    solve = setup.solve(120)
+    print('status: ')
+    print(solve)
+    if solve.getStatus() == ob.PlannerStatus.EXACT_SOLUTION:
         # simplify & print the solution
         setup.simplifySolution()
         path = setup.getSolutionPath()
         path_print = path.printAsMatrix()
+
         f = open('data/paths/path_%d.txt' % (i), 'w')
+
         f.write(path_print)
         f.close()
